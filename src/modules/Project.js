c867b6e1e79d5e5ed9c912d080c3ca1e607e6819
marvka <marvka@tutanota.com>
2021-06-18 16:30:54 +0100

    Add Project module

---
+++ src/modules/Project.js
@@ -0,0 +1,12 @@
+export default function createProject(title) {
+  const todos = [];
+  const getTitle = () => title;
+  const addTodo = (todo) => todos.push(todo);
+  const deleteTodo = (title) => {
+    const index = todos.findIndex((todo) => todo.getTitle() == title);
+    todos.splice(index, 1);
+  };
+  const getTodos = () => todos;
+
+  return { getTitle, addTodo, deleteTodo, getTodos };
+}
