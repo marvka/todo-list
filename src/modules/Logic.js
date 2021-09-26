2b4efc60a96d7ab2011126fe1ac8a16e1ce02793
marvka <marvink@posteo.net>
2021-09-26 15:16:14 +0100

    Condense lines in init function

--- src/modules/Logic.js
+++ src/modules/Logic.js
@@ -5,10 +5,8 @@ import Todo from './Todo';
 
 const init = () => {
   const inbox = Project('Inbox');
-  const greetingTodo = Todo('This is a todo');
-  inbox.addTodo(greetingTodo);
-  const helpTodo = Todo('Feel free to add your own projects and todos!');
-  inbox.addTodo(helpTodo);
+  inbox.addTodo(Todo('This is a todo'));
+  inbox.addTodo(Todo('Feel free to add your own projects and todos!'));
   Database.addProject(inbox);
 };
 
