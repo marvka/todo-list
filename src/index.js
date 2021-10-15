02f3a593786fc89a21e09472bfdcbc1cd749a0a2
marvka <marvink@posteo.net>
2021-10-15 07:00:56 +0100

    Remove file that didn't make sense
    I don't need a Logic.js file since all it does is init the database and
    get the UI to load. Might as well just keep those few lines in index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 // Rewrite modules to only use exports and not createSomething style
 import './style.css';
-import initialize from './modules/logic/Logic';
+import Database from './modules/logic/Database';
+import * as UI from './modules/ui/UI';
+import Project from './modules/logic/Project';
+import Todo from './modules/logic/Todo';
 
-initialize();
+const init = () => {
+  const inbox = Project('Inbox');
+  inbox.addTodo(Todo('This is a todo'));
+  inbox.addTodo(Todo('Feel free to add your own projects and todos!'));
+  Database.addProject(inbox);
+};
+
+if (localStorage.length) {
+  Database.load();
+} else {
+  init();
+}
+UI.init();
